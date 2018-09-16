35b3ed8f9d548c33bbfeee36cf22063a9255c1e3
saguins <saralara555@gmail.com>
2018-09-16 15:10:10 +0700

    saguicode

--- src/main.js
+++ src/main.js
@@ -1,26 +1,30 @@
 import 'phaser';
+import MainMenu from './MainMenu';
 import Test from './Test';
 import socket from './socket';
 
 const WIDTH = document.body.clientWidth;
-const HEIGHT = screen.height*0.9; 
+const HEIGHT = screen.height * 0.9;
 let config = {
     type: Phaser.WEBGL,
     parent: 'content',
-    width: WIDTH,
-    height: HEIGHT,
+    width: 800,
+    height: 600,
     scaleMode: 0, // Phaser.ScaleManager.EXACT_FIT,
     physics: {
         default: 'arcade',
-        arcade : {
+        arcade: {
+            gravity: { y: 300 },
             debug: false
         }
     },
     scene: [
+        MainMenu,
         Test
     ]
 };
-socket.on('news', (data) => {
-    console.log(data)
- });
+socket.on('news', (data) => 
+{
+    console.log(data);
+});
 let game = new Phaser.Game(config);
